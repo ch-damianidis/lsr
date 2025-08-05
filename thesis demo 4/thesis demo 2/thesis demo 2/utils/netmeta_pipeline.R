0023c302a8a6dfdd1906505c87e1932c7c644483
ch-damianidis <charalampos.damianidis@gmail.com>
2025-08-05 16:04:17 +0300

    First commit - R project upload

---
+++ thesis demo 4/thesis demo 2/thesis demo 2/utils/netmeta_pipeline.R
@@ -0,0 +1,45 @@
+library(netmeta)
+library(igraph)
+
+run_cnma_analysis <- function(data, interaction = FALSE, random = TRUE) {
+  # Check if network is connected
+  edges <- data.frame(from = data$treat1, to = data$treat2)
+  g <- igraph::graph_from_data_frame(edges, directed = FALSE)
+  connected <- igraph::is.connected(g)
+  
+  if (connected) {
+    # Connected: netcomb for additive or interaction CNMA
+    nm <- netmeta(
+      TE = data$logHR,
+      seTE = data$selogHR,
+      treat1 = data$treat1,
+      treat2 = data$treat2,
+      studlab = data$study,
+      sm = "HR",
+      random = random
+    )
+    nc <- netcomb(
+      nm,
+      ia.model = interaction # TRUE=interaction, FALSE=additive
+    )
+    attr(nc, "forced_additive") <- FALSE
+    return(nc)
+  } else {
+    # Disconnected: Only discomb (additive CNMA) 
+    if (interaction) {
+      warning("Interaction model is not available for disconnected networks. Using additive model instead.")
+    }
+    dc <- discomb(
+      TE = data$logHR,
+      seTE = data$selogHR,
+      treat1 = data$treat1,
+      treat2 = data$treat2,
+      studlab = data$study,
+      data = data,
+      sm = "HR",
+      random = random
+    )
+    attr(dc, "forced_additive") <- interaction # TRUE αν ζήτησε interaction
+    return(dc)
+  }
+}
