fe31e841025f0e4f9c97e347c1b213e6c6de30d8
ch-damianidis <charalampos.damianidis@gmail.com>
2025-09-04 00:24:44 +0300

    Last Thesis Update

---
+++ Copyfinallsr/lsr/utils/netmeta_pipeline.R
@@ -0,0 +1,60 @@
+library(netmeta)
+library(igraph)
+
+run_cnma_analysis <- function(data, interaction = FALSE, random = TRUE) {
+  # Build graph to check if the treatment network is connected
+  edges <- data.frame(from = data$treat1, to = data$treat2)
+  g <- igraph::graph_from_data_frame(edges, directed = FALSE)
+  connected <- igraph::is_connected(g)
+  
+  if (connected) {
+    # If the network is connected: run netmeta + netcomb (CNMA)
+    nm <- netmeta(
+      TE     = data$logHR,
+      seTE   = data$selogHR,
+      treat1 = data$treat1,
+      treat2 = data$treat2,
+      studlab= data$study,
+      sm     = "HR",
+      random = random
+    )
+    
+    if (interaction) {  # Extract all unique components from treatment names
+      
+      all_trts <- unique(c(data$treat1, data$treat2))
+      comps <- sort(unique(trimws(unlist(strsplit(all_trts, "\\s*\\+\\s*")))))
+      
+      # Generate all 2-way interaction terms in the correct format ("X+Y") [SOS]
+      comb.ia <- combn(comps, 2, FUN = function(x) paste(x, collapse = "+"))
+      # Create C-matrix including interaction terms
+      Cmat_int <- createC(nm, comb.ia = comb.ia, inactive = "Clb")
+      
+      nc <- netcomb(nm, C.matrix = Cmat_int)
+    } else {  # Run CNMA with interactions
+      nc <- netcomb(
+        nm,
+        sep.trts = " + "
+      )
+    }
+    
+    return(nc)
+    # If the network is disconnected: use discomb (only additive CNMA possible)
+  } else {
+    
+    if (interaction) {
+      warning("Interaction model is not available for disconnected networks. Using additive model instead.")
+    }
+    dc <- discomb(
+      TE     = data$logHR,
+      seTE   = data$selogHR,
+      treat1 = data$treat1,
+      treat2 = data$treat2,
+      studlab= data$study,
+      data   = data,
+      sm     = "HR",
+      random = random
+    )
+    attr(dc, "forced_additive") <- interaction  #Mark that interaction was requested but forced to additive due to disconnection
+    return(dc)
+  }
+}
